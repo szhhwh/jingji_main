cd5f15db84b257031a01561dd8b617446ed39417
王浩 <3116737282@qq.com>
2025-01-05 13:23:31 +0800

    添加页脚

---
+++ src/FooTer.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const year = ref(new Date().getFullYear())
+</script>
+
+<template>
+  <div class="footer-wrapper">
+    <div class="footer">版权所有 &copy; 2023-{{ year }} 荆棘鸟文学社</div>
+  </div>
+</template>
+
+<style scoped>
+.footer-wrapper {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  display: flex;
+  justify-items: center;
+  justify-content: space-evenly;
+  align-items: center;
+  .footer {
+    font-size: 14px;
+    margin-top: 8px;
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-bottom: 8px;
+    display: block;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import FooTer from './FooTer.vue'
 
 createApp(App).mount('#app')
+createApp(FooTer).mount('#footer')
