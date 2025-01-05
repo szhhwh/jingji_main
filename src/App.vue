515807f5d09d12d0aca610fcdcebb2d569aa7da9
王浩 <3116737282@qq.com>
2025-01-05 13:24:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheList from './components/TheList.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.webp" width="125" height="125" />
+    <img alt="Jingji logo" class="logo" src="./assets/logo.webp" width="125" height="125" />
 
     <div class="wrapper">
       <Hero msg="荆棘鸟文学社" />
